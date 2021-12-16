f415b288ba0a560c025c9bee266cb5368c3db2d2
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-12-16 16:43:59 -0600

    Finished Controllers exercises for SpringBoot

---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String mainPage(){
+        return "This is the index page";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String userPage(@PathVariable int id){
+        return "This is user " + id + "'s profile page";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String createPost(){
+        return "View the form for creating a post!";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String addPost(){
+        return "Create a new post!";
+    }
+}
