f4e8bb1e3db521a2f4c7eb7ab5181c5d98f5fbb0
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2022-01-03 13:36:20 -0600

    finished Spring views lecture

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -25,4 +26,14 @@ public class HelloController {
         return number + " plus one is " + (number + 1) + "!";
     }
 
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
 }
