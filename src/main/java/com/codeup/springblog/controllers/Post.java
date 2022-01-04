83fa1e6e450b1696c15f078cb0f02f6cfb35cd2e
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2022-01-04 11:55:55 -0600

    working on last part of JPA exercises. Taking break

--- src/main/java/com/codeup/springblog/controllers/Post.java
+++ src/main/java/com/codeup/springblog/controllers/Post.java
@@ -1,8 +1,21 @@
 package com.codeup.springblog.controllers;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Column(nullable = false)
     private String body;
+
+    @Column(nullable = false, length = 50)
     private String title;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Post() {
     }
@@ -14,6 +27,12 @@ public class Post {
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
+    }
+
+    public Post(String body, String title, Long id) {
+        this.body = body;
+        this.title = title;
+        this.id = id;
     }
 
     public String getBody() {
@@ -30,5 +49,13 @@ public class Post {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
---
+++ src/main/java/com/codeup/springblog/controllers/PostsController.java
@@ -0,0 +1,23 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostsController {
+
+    private final PostRepository postDao;
+
+    public PostsController(PostRepository postDao){
+        this.postDao = postDao;
+    }
+
+    @GetMapping("/posts")
+    public String postsIndex(Model model){
+        model.addAttribute("cars", postDao.findAll());
+
+        return "posts";
+
+    }
+}
